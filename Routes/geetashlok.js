dec7831d39dcd294700404b9f9b7623face4303a
Pushkar155 <96383324+Pushkar155@users.noreply.github.com>
2023-07-20 13:44:53 +0530

    adding all new routes

---
+++ Routes/geetashlok.js
@@ -0,0 +1,18 @@
+const router=require("express").Router();
+const GeetaShlokschema = require("../Schema/GeetaShlok");
+
+router.post("/addShlok",async(req,res)=>{
+    const newGeetaShlok=new GeetaShlokschema(req.body);
+    try {
+        const saveShlok=await newGeetaShlok.save();
+        res.status(200).json(saveShlok);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.get("/getShlok",async (req,res)=>{
+    const shlok =await GeetaShlokschema.find();
+    res.status(200).send(shlok);
+})
+module.exports=router;
